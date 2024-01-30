2299b01a1009f0e8afdb83372c61a451b2c89c04
mr-munshi <realmrmunshi@gmail.com>
2024-01-30 19:48:09 +0600

    Changed background text placement and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen items-center">
+    <div className="grid min-h-screen w-full place-items-center bg-[#0a0c10] text-white">
       <div>Hello World!</div>
     </div>
   );
